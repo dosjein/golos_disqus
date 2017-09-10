3b8acd45bc6104ee80ca6f044849a80a9471d841
Ronalds Sovas <sovas@dosje.in>
2017-09-10 12:50:48 +0300

    added bootstrap comments sample

--- media/js/golos.js
+++ media/js/golos.js
@@ -7,7 +7,9 @@ jQuery( document ).ready(function() {
 	});
 
 	jQuery('.btn post-action__button').click(function(){
-
+		golos.broadcast.comment(wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, function(err, result) {
+		console.log(err, result);
+		});
 	});
 
 	jQuery(".login_form").submit(function(e){
@@ -27,17 +29,22 @@ jQuery( document ).ready(function() {
 });
 
 function execGolos(){
-	golos.api.getAccounts([window.name , window.pass], function(err, response){
+	golos.api.getAccounts([window.name], function(err, response){
 
-		console.log(window.name + '&&' + window.pass );
+		golos.api.login(window.name, window.pass , function(err, result) {
 
-		if (response.length > 0){
-		    jQuery('.login_form').addClass('hidden');
-		    jQuery('#disqus_thread').show();
-		}else{
-		    console.log('response ned, dan');
-		    jQuery('#disqus_thread').hide();
-		    jQuery('.login_form').removeClass('hidden');
-		}
+			console.log('login code');
+			console.log(result);
+
+			if (result.length > 0){
+			    jQuery('.login_form').addClass('hidden');
+			    jQuery('#disqus_thread').show();
+			}else{
+			    console.log('response ned, dan');
+			    jQuery('#disqus_thread').hide();
+			    jQuery('.login_form').removeClass('hidden');
+			}
+		});
+
 	});
 }
